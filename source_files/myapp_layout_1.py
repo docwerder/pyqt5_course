0062a05c9455766981a3a0a46541d984ecf7ccc3
docwerder <wingbermuehle@icloud.com>
2022-03-13 12:04:10 +0100

    V1.13 pyside2 source_files added

--- source_files/myapp_layout_1.py
+++ source_files/myapp_layout_1.py
@@ -1,24 +1,50 @@
 import sys
+sys.path.append('/Users/joerg/repos')
 from PyQt6.QtCore import QSize, Qt
 import PyQt6.QtWidgets
-from PyQt6.QtWidgets import * # QApplication, QMainWindow, QPushButton
+from PySide6.QtWidgets import \
+    QApplication, QGridLayout, QHBoxLayout, QPushButton, QVBoxLayout, QWidget, QMainWindow
+from pyside6_source.basic.layout_colorwidget import Color #as COlor
 
-# Subclass QMainWindow to customize your application's main window class MainWindow(QMainWindow):
 
 class MainWindow(QMainWindow):
     def __init__(self):
         super().__init__()
 
         self.setWindowTitle("My App")
-        self.button = QPushButton("Press Me!")
-        self.setMinimumSize(QSize(400, 300))
+
+        layout = QGridLayout()
+        layout.addWidget(Color("red"), 0, 0)
+        layout.addWidget(Color("green"), 1, 1)
+        layout.addWidget(Color("yellow"), 2, 0)
+        layout.addWidget(Color("green"), 3, 1)
 
 
+        btn_layout = QHBoxLayout()
+        btn_layout.addWidget(QPushButton('BTN 1'))
+        btn_layout.addWidget(QPushButton('BTN 2'))
+
+        layout_all = QVBoxLayout()
+        layout_all.addLayout(layout)
+        layout_all.addLayout(btn_layout)
+
+        widget = QWidget()
+        widget.setLayout(layout_all)
+        self.setCentralWidget(widget)
+        # btn_layout.addWidget(QLabel('Label 2'))
+        # color_layout.addWidget(COlor("red"))
+
+        #my_layout.addWidget(Color("red"))
+
+        # dummy_widget = QWidget()
+        # dummy_widget.setLayout(color_layout)
+        # self.setCentralWidget(dummy_widget)
         # Set the central widget of the Window.
-        self.setCentralWidget(self.button)
+        #self.setCentralWidget(self.button)
 
 if __name__== '__main__':
     app = QApplication(sys.argv)
+    print(sys.path)
     window = MainWindow()
     window.show()
     sys.exit(app.exec())
--- source_files/stacked_layout_test.py
+++ source_files/stacked_layout_test.py
@@ -1,5 +1,7 @@
-from PyQt5.QtWidgets import QStackedLayout  # add this import
-
+import sys
+from PyQt5.QtWidgets import QStackedLayout
+from PyQt6.QtWidgets import QWidget, QMainWindow, QApplication # add this import
+from pyside6_source.basic.layout_colorwidget import Color
 
 class MainWindow(QMainWindow):
     def __init__(self):
@@ -19,3 +21,9 @@ class MainWindow(QMainWindow):
         widget = QWidget()
         widget.setLayout(layout)
         self.setCentralWidget(widget)
+
+if __name__== '__main__':
+    app = QApplication(sys.argv)
+    window = MainWindow()
+    window.show()
+    sys.exit(app.exec())
